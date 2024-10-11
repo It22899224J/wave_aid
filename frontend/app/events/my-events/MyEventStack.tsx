a78ef37f080cddc779caff3f2f4a62651bf2a002
Hirusha Jayarathne <hirushachamod2@gmail.com>
2024-10-12 00:59:32 +0530

    Refactor booking navigation and related components

--- frontend/app/events/my-events/MyEventStack.tsx
+++ frontend/app/events/my-events/MyEventStack.tsx
@@ -10,8 +10,11 @@ import MyEvents from "./MyEvents";
 import OrganizedEventsPast from "../update-event/OrganizedEventsPast";
 import PastEvents from "../past-events/PastEvents";
 import PastEventDetails from "../past-events/PastEventDetails";
-import { Booking } from "../book-a-seat/BookingNav";
 import MyEventsMainView from "./MyEventsMainView";
+import BookingCanceledScreen from "../book-a-seat/BookingCanceled";
+import BookingConfirmation from "../book-a-seat/BookingConfirmation";
+import SeatBooking from "../book-a-seat/BusLayout";
+import SelectBus from "../book-a-seat/SelectBus";
 const Stack = createStackNavigator();
 
 export const MyEventStack = () => {
@@ -83,6 +86,27 @@ export const MyEventStack = () => {
         component={MyEvents}
         options={{ headerTitle: "MyEvents", headerTitleAlign: "center" }}
       />
+        <Stack.Screen
+                name="SelectBus"
+                component={SelectBus}
+                options={{ headerTitle: "Select Bus" }}
+            />
+            <Stack.Screen
+                name="BusLayout"
+                component={SeatBooking}
+                options={{ headerTitle: "Bus Layout" }}
+            />
+            <Stack.Screen
+                name="BookingConfirmation"
+                component={BookingConfirmation}
+                options={{ title: "BookingConfirmation" }}
+            />
+            <Stack.Screen
+                name="BookingCanceled"
+                component={BookingCanceledScreen}
+                options={{ title: "BookingCanceled" }}
+            />
+
     </Stack.Navigator>
   );
 };
